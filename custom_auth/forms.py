bbdd457c9a168781650de18a0c79bd9415f65cc1
exicap <arsenijtisenko1@gmail.com>
2025-03-16 20:15:15 +0000

    Updated auth page

---
+++ custom_auth/forms.py
@@ -0,0 +1,19 @@
+#этот файл это форма для регистрации пользователя, которая наследуется от UserCreationForm
+#чтобы использовать встроенные методы валидации формы Django
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User  #Custom user model from models.py
+from django_recaptcha.fields import ReCaptchaField
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True) # making email field required
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2'] # fields to be displayed in the form 
+                                                                 # password1 and password2 are for password and password confirmation
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass  # Оставляем пустой 
